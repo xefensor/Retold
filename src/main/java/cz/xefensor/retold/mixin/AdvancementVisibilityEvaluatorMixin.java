d4cd4b82353de3022201da1c3e8886a784ec57cc
xefensor <acc@alexejtusl.cz>
2026-06-30 19:05:57 +0200

    Make advancement visibility mixin fail-safe

--- src/main/java/cz/xefensor/retold/mixin/AdvancementVisibilityEvaluatorMixin.java
+++ src/main/java/cz/xefensor/retold/mixin/AdvancementVisibilityEvaluatorMixin.java
@@ -1,5 +1,6 @@
 package cz.xefensor.retold.mixin;
 
+import cz.xefensor.retold.Retold;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.server.advancements.AdvancementVisibilityEvaluator;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,7 +10,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AdvancementVisibilityEvaluator.class)
 public abstract class AdvancementVisibilityEvaluatorMixin {
-    private static final Object RETOLD_HIDE_RULE = retold$getVisibilityRule("HIDE");
+    private static final Object RETOLD_HIDE_RULE =
+            retold$findVisibilityRule("HIDE");
+
+    private static final boolean RETOLD_ADVANCEMENT_HIDING_ENABLED =
+            RETOLD_HIDE_RULE != null;
 
     @Inject(
             method = "evaluateVisibilityRule(Lnet/minecraft/advancements/Advancement;Z)Lnet/minecraft/server/advancements/AdvancementVisibilityEvaluator$VisibilityRule;",
@@ -22,26 +27,53 @@ public abstract class AdvancementVisibilityEvaluatorMixin {
             boolean isDone,
             CallbackInfoReturnable cir
     ) {
-        if (!isDone) {
-            cir.setReturnValue(RETOLD_HIDE_RULE);
+        if (isDone) {
+            return;
         }
+
+        if (!RETOLD_ADVANCEMENT_HIDING_ENABLED) {
+            return;
+        }
+
+        cir.setReturnValue(RETOLD_HIDE_RULE);
     }
 
-    private static Object retold$getVisibilityRule(String name) {
+    private static Object retold$findVisibilityRule(String name) {
         try {
-            Class<?> ruleClass = Class.forName(
+            Class ruleClass = Class.forName(
                     "net.minecraft.server.advancements.AdvancementVisibilityEvaluator$VisibilityRule"
             );
 
-            for (Object constant : ruleClass.getEnumConstants()) {
-                if (constant instanceof Enum<?> enumConstant && enumConstant.name().equals(name)) {
+            Object[] constants = ruleClass.getEnumConstants();
+
+            if (constants == null) {
+                Retold.LOGGER.warn(
+                        "Could not enable advancement hiding: VisibilityRule is not an enum"
+                );
+
+                return null;
+            }
+
+            for (Object constant : constants) {
+                if (constant instanceof Enum enumConstant
+                        && enumConstant.name().equals(name)) {
                     return constant;
                 }
             }
 
-            throw new IllegalStateException("Missing AdvancementVisibilityEvaluator.VisibilityRule." + name);
-        } catch (ClassNotFoundException exception) {
-            throw new IllegalStateException("Could not find AdvancementVisibilityEvaluator.VisibilityRule", exception);
+            Retold.LOGGER.warn(
+                    "Could not enable advancement hiding: missing VisibilityRule.{}",
+                    name
+            );
+
+            return null;
+        } catch (ReflectiveOperationException | LinkageError exception) {
+            Retold.LOGGER.warn(
+                    "Could not enable advancement hiding. Retold will leave advancement visibility unchanged.",
+                    exception
+            );
+
+            return null;
         }
     }
 }
